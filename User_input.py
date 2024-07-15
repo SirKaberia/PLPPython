a401fbc942a2491180120a7d35b5da275c4fa895
SirKaberia <kaberialewis@gmail.com>
2024-07-15 15:56:09 +0300

    First commit

---
+++ User_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Asking for the user's name
+name = input("Please enter your name: ")
+
+# Asking for the user's age
+age = input("Please enter your age: ")
+
+# Asking for the user's location
+location = input("Please enter your location: ")
+
+# Printing out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
